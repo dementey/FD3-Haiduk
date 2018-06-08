809dbaa26cff7f4b1c2eaa9b30dc274bd8084c25
dementey <dementeyyy@gmail.com>
2018-06-08 15:53:29 +0300

    ready filter

---
+++ filter/filter.js
@@ -0,0 +1,47 @@
+﻿var filterComponent = React.createClass({
+
+    displayName: 'filterComponent',
+
+    propTypes: {
+        defaultList: React.PropTypes.arrayOf(React.PropTypes.string)
+    },
+    //иницилизируем стейты
+    getInitialState: function () {
+        return {
+            statelist: this.props.defaultList,
+            stateInputText: '',
+            checkedCheckbox: false,
+        }
+    },
+    //функция отработки событий ввода текста
+    inputTextChange: function (EO) {
+        this.changeStatelist(EO.target.value, this.state.checkedCheckbox);
+    },
+    //функция отработки событий изменения чекбокса
+    checkboxChecked: function (EO) {
+        this.changeStatelist(this.state.stateInputText, EO.target.checked);
+    },
+    //функция сортировки массива в зависомости от стейтов
+    changeStatelist: function (inputText, checked) {
+        var newStateList = this.props.defaultList.filter((v) => v.includes(inputText));
+
+        checked
+            ? newStateList.sort()
+            : null;
+
+        this.setState({ statelist: newStateList, stateInputText: inputText, checkedCheckbox: checked, });
+    },
+    //рендеринг
+    render: function () {
+        var renderlist = this.state.statelist.map(function (v) { return React.DOM.li({ key: v }, v) });
+        return React.DOM.div({ className: "filter" },
+            React.DOM.label(null,
+                'Cортировать',
+                React.DOM.input({ type: 'checkbox', onClick: this.checkboxChecked }, null)),
+            React.DOM.label(null,
+                'Найти',
+                React.DOM.input({ type: 'text', onChange: this.inputTextChange }, null)),
+            React.DOM.div({ className: "list" }, renderlist)
+        );
+    }
+});
---
+++ ishop2/ishop.js
@@ -0,0 +1,62 @@
+﻿var ListOfProducts = React.createClass({
+
+
+  displayName: 'ListOfProducts',
+
+
+  propTypes: {
+    products: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        code: React.PropTypes.number.isRequired,
+        name: React.PropTypes.string.isRequired,
+        price: React.PropTypes.number.isRequired,
+        url: React.PropTypes.string.isRequired,
+        amount: React.PropTypes.number.isRequired,
+      })
+    ),
+    title: React.PropTypes.shape({
+      code: React.PropTypes.number.isRequired,
+      name: React.PropTypes.string.isRequired,
+      price: React.PropTypes.string.isRequired,
+      url: React.PropTypes.string.isRequired,
+      amount: React.PropTypes.string.isRequired,
+    })
+
+  },
+
+
+  render: function () {
+
+    var product = this.props.products;
+
+    var title = this.props.title;
+
+    var productsCode = React.DOM.table({ className: 'ProductTable' },
+      React.DOM.thead({ key: title.code, className: 'Title' },
+        React.DOM.tr(null,
+          React.DOM.td(null, title.name),
+          React.DOM.td(null, title.url),
+          React.DOM.td(null, title.price),
+          React.DOM.td(null, title.amount), ), ),
+      React.DOM.tbody({ className: 'Products' },
+        product.map(function (v, i) {
+          return React.DOM.tr({ key: v.code, className: 'Product' + v.code },
+            React.DOM.td(null, v.name),
+            React.DOM.td(null, React.DOM.a({ href: v.url }, 'сcылка на фото')),
+            React.DOM.td(null, v.price),
+            React.DOM.td(null, v.amount),
+          )
+        })
+      )
+    );
+
+    return React.DOM.div(null,
+      React.DOM.div({ className: 'Shop' }, this.props.shop),
+      React.DOM.div(null, productsCode),
+    );
+
+  },
+
+
+
+});
