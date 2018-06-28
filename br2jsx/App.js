9fbfa3ea654134f211a619840a525678b3cf66da
dementey <dementeyyy@gmail.com>
2018-06-28 23:42:45 +0300

    br2jsx

---
+++ br2jsx/App.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Br2jsx from './components/br2jsx';
+
+let textJSON = require('./text.json');
+
+ReactDOM.render(
+  <Br2jsx
+    text={textJSON} />,
+  document.getElementById('container')
+);
---
+++ br2jsx/components/br2jsx.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './br2jsx.css';
+
+class Br2jsx extends React.Component {
+
+  static propTypes = {
+    text: PropTypes.shape({
+      content: PropTypes.string.isRequired,
+    })
+  };
+
+  br2jsxFunc(txt) {
+    let arr = txt.split(/<br\s*\/?>/i);
+    return arr.reduce((el, a) => el.concat(a, <br />), []);
+  };
+
+  render() {
+    return (
+      <div className="br2jsx">
+        {this.br2jsxFunc(this.props.text.content)}
+      </div>
+    );
+  }
+  
+}
+
+export default Br2jsx;
