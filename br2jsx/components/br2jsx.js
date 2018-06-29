f49303ac0b58830959bb6e9e493102c5df338dc3
dementey <dementeyyy@gmail.com>
2018-06-29 14:31:45 +0300

    br2jsx remarks

--- br2jsx/components/br2jsx.js
+++ br2jsx/components/br2jsx.js
@@ -12,18 +12,20 @@ class Br2jsx extends React.Component {
   };
 
   br2jsxFunc(txt) {
+    let keyNum=1;
     let arr = txt.split(/<br\s*\/?>/i);
-    return arr.reduce((el, a) => el.concat(a, <br />), []);
+    return arr.reduce((v, i) => v.concat(i, <br key={keyNum++}/>), []);
   };
 
   render() {
+    console.log(this.br2jsxFunc(this.props.text.content));
     return (
       <div className="br2jsx">
         {this.br2jsxFunc(this.props.text.content)}
       </div>
     );
   }
-  
+
 }
 
 export default Br2jsx;
